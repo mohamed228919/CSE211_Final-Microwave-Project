d4e74fd3fb2e415119e0817f2bedcca503c3c55d
mohamedmostaf21 <87494262+mohamedmostaf21@users.noreply.github.com>
2022-05-26 00:05:40 +0200

    name of function SW is capital

--- interrupt.c
+++ interrupt.c
@@ -15,22 +15,22 @@ int flag1 =1;
 void GPIOF_Handler(void){
 	int i;
 	flag1 = 1;
-	while(!sw1_Input()){};				//pause
+	while(!SW1_Input()){};				//pause
 		RGB_Output(RED+BLUE+GREEN);
 		while(flag1){
 			GPIO_PORTF_DATA_R ^= RED+GREEN+BLUE;
 			for(i=0;i<300000 && flag1;i++) 			//for delay because systick wait is an interrupt
 			{
-				if(!sw2_Input()){
-					while(!sw2_Input()){}
+				if(!SW2_Input()){
+					while(!SW2_Input()){}
 				GPIO_PORTF_ICR_R |= 0x10;
 				RGB_Output(RED+BLUE+GREEN);
 				flag1 = 0;
 				break;
 					}
-			else if(!sw1_Input())
+			else if(!SW1_Input())
 			{
-				while(!sw1_Input()){}
+				while(!SW1_Input()){}
 				flag_stop();
 				RGB_Output(RED+BLUE+GREEN);
 				flag1 = 0;
