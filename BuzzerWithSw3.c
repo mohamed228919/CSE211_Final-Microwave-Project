72365cf350a40a31a2afb796ad577ec25a24b6c5
mohamedmostaf21 <87494262+mohamedmostaf21@users.noreply.github.com>
2022-05-25 23:58:41 +0200

    Update BuzzerWithSw3.c
    Ma

--- BuzzerWithSw3.c
+++ BuzzerWithSw3.c
@@ -15,6 +15,7 @@
 */
 void SW3WithBuzzer_Init(void)
 {
+
 	SYSCTL_RCGCGPIO_R |= 0x01;           							//activate clock for PORTA
   while((SYSCTL_PRGPIO_R & 0x01)==0){}           		//Check if PORTA is activated or not
 	GPIO_PORTA_LOCK_R = 0x4C4F434B;           				//unlock GPIO of PORTA
@@ -30,8 +31,9 @@ void SW3WithBuzzer_Init(void)
   GPIO_PORTA_AMSEL_R &=~ 0x08;        							//Disable analog function on A3 of PORTA
 	GPIO_PORTA_AFSEL_R &=~ 0x08;        							//Disable alternate function on A3 of PORTA
 
- 
-	GPIO_PORTA_IS_R &= ~0x04;                 // PA4 is low level event
+
+    //Interrupt for switch 3
+  GPIO_PORTA_IS_R &= ~0x04;                 // PA4 is low level event
   GPIO_PORTA_IEV_R &= ~0x00;               // PA4 level event
   GPIO_PORTA_ICR_R = 0x04;                // clear flag4
   GPIO_PORTA_IM_R |= 0x04;               // arm interrupt on PA4 
@@ -46,10 +48,8 @@ unsigned char SW3_Input(void)
   return (GPIO_PORTA_DATA_R & 0x08);
 }
 
-
 //Turn ON, Turn OFF Buzzer
 void Buzzer_Output(unsigned char data)
 {
   GPIO_PORTA_DATA_R = (GPIO_PORTA_DATA_R & 0x0B) + data;
 }
-
